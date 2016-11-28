024c2c8aaae2562ed667c97f2160b4ddf09c9f51
Ramakrishna Allam <ramakrishna.allam@tantalumcorporation.com>
2016-11-28 21:27:12 +0000

    refactoring - adding prices val as HashMap

--- src/main/scala/com/hmrc/exercise/CheckoutApp.scala
+++ src/main/scala/com/hmrc/exercise/CheckoutApp.scala
@@ -1,8 +1,11 @@
 package com.hmrc.exercise
 
+import scala.collection.immutable.HashMap
+
 class CheckoutApp {
+  val prices = HashMap("Apple" -> 60, "Orange" -> 25)
   def checkout(lisOfItems: List[String]):String = {
     val (apples, oranges) = lisOfItems.partition(item => item.equals("Apple"))
-    "£" + (apples.length * 60 + oranges.length * 25)/100.toDouble
+    "£" + (apples.length * prices("Apple") + oranges.length * prices("Orange"))/100.toDouble
   }
 }
