39fdc6464ed5487484b50d671bdf9486dff2469d
Ramakrishna Allam <ramakrishna.allam@tantalumcorporation.com>
2016-11-28 21:47:35 +0000

    basic test cases and code

--- src/main/scala/com/hmrc/exercise/CheckoutAppWithOffers.scala
+++ src/main/scala/com/hmrc/exercise/CheckoutAppWithOffers.scala
@@ -1,5 +1,22 @@
 package com.hmrc.exercise
 
-class CheckoutAppWithOffers extends Application{
-  def checkout(listOfItems: List[String]): String = "£0.0"
+import scala.collection.immutable.HashMap
+
+class CheckoutAppWithOffers extends Application {
+  val offers = HashMap("Apple" ->(2, 1), "Orange" ->(3, 2))
+  val prices = HashMap("Apple" -> 60, "Orange" -> 25)
+
+  def checkout(listOfItems: List[String]): String = {
+    val (apples, oranges) = listOfItems.partition(s => s.equals("Apple"))
+    val (applesCount, orangesCount) = (apples.length, oranges.length)
+
+    val (effectiveAppleCount, effectiveOrangeCount) = {
+      val tApples = offers.getOrElse("Apple", (1, 1))
+      val effectiveAppleCount = (applesCount / tApples._1) * tApples._2 + (applesCount % tApples._1)
+      val tOranges = offers.getOrElse("Orange", (1, 1))
+      val effectiveOrangeCount = (orangesCount / tOranges._1) * tOranges._2 + (orangesCount % tOranges._1)
+      (effectiveAppleCount, effectiveOrangeCount)
+    }
+    "£" + (effectiveAppleCount * prices("Apple") + effectiveOrangeCount * prices("Orange")) / 100.toDouble
+  }
 }
--- src/test/scala/com/hmrc/exercise/CheckoutAppWithOffersSpec.scala
+++ src/test/scala/com/hmrc/exercise/CheckoutAppWithOffersSpec.scala
@@ -11,4 +11,17 @@ class CheckoutAppWithOffersSpec extends FunSuite with BeforeAndAfter{
   test("An Empty List of items should return £0.0"){
     assert(application.checkout(Nil) == "£0.0")
   }
+
+  test ("Only Apples case") {
+    assert (application.checkout(List("Apple", "Apple", "Apple", "Apple")) == "£1.2")
+  }
+
+  test ("Only Oranges case") {
+    assert (application.checkout(List("Orange", "Orange")) == "£0.5")
+  }
+
+  test ("Three Oranges case") {
+    assert (application.checkout(List("Orange", "Orange", "Orange")) == "£0.5")
+  }
+
 }
