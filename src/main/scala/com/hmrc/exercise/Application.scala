7666f5013e9c4e9a8be9e38781a3e963f7e56162
Ramakrishna Allam <ramakrishna.allam@tantalumcorporation.com>
2016-11-28 21:40:54 +0000

    Adding comments to the trait method to be more clear

--- src/main/scala/com/hmrc/exercise/Application.scala
+++ src/main/scala/com/hmrc/exercise/Application.scala
@@ -1,9 +1,6 @@
 package com.hmrc.exercise
 
-/**
-  * Created by Ramakrishna Allam on 28/11/2016.
-  */
 trait Application {
-
+  //returns the cost of the cart in String format prepeding "£"
   def checkout(listOfItems: List[String]): String
 }
